0f844e264600c9e85de52333a36e8952d26ac5a3
MF-B <78307860+MF-B@users.noreply.github.com>
2025-07-26 15:56:42 +0000

    Deploying to gh-pages from @ Nyx-Tron/freeos@0deffde6a136be53bb04ee5210d709ffe1bf5829 🚀

---
+++ starry_core/file/proc/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["init_procfs"],"mod":["selfs"]};
---
+++ trait.impl/axfs_vfs/trait.VfsNodeOps.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["starry_core",[["impl VfsNodeOps for <a class=\"struct\" href=\"starry_core/file/proc/selfs/struct.SelfExe.html\" title=\"struct starry_core::file::proc::selfs::SelfExe\">SelfExe</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[187]}
